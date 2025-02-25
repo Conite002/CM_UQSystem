aacfa6adc3e07bb5b351d89e7c93e0ce308b1520
Conite002 <conite.gbodogbe@imsp-uac.org>
2025-02-25 13:18:46 +0000

    Add CIFAR-10 data loading function and generalize dataset loading

--- data/load_data.py
+++ data/load_data.py
@@ -13,6 +13,18 @@ def load_mnist(batch_size=128):
 
     return train_loader, test_loader
 
-# if __name__ == "__main__":
-#     train_loader, test_loader = load_mnist()
-#     print(f"Train batch: {next(iter(train_loader))[0].shape}")
+def load_cifar10(batch_size=128):
+    transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+    return train_loader, test_loader
+    
+def load_dataset(dataset, batch_size=128, train=True):
+    if dataset == "mnist":
+        return load_mnist(batch_size)
+    elif dataset == "cifar10":
+        return load_cifar10(batch_size)
+    else:
+        raise ValueError(f"Unknown dataset: {dataset}")
